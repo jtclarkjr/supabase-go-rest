1e613c4d289197f5c9a74892f3888bcb0591fbf5
James <jtclarkjr@ucla.edu>
2024-07-11 21:11:26 +0900

    update example

--- example/example.go
+++ example/example.go
@@ -1,145 +1,229 @@
-package main
+// Example pulled from working project
 
-// import (
-// "encoding/json"
-// "fmt"
-// "github.com/jtclarkjr/supabase-go-rest"
-// "net/http"
+package example
 
-// "github.com/go-chi/chi/v5"
-// "github.com/go-chi/chi/v5/middleware"
-// )
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 
-// var (
-// 	supabaseBaseUrl = "https://your-project.supabase.co"
-// 	supabaseKey     = "your-supabase-api-key"
-// )
+	"github.com/jtclarkjr/supabase-go-rest"
 
-// func getFoodHandler(w http.ResponseWriter, r *http.Request) {
-// 	token := r.Header.Get("Authorization")
-// 	if token == "" {
-// 		http.Error(w, "Authorization token missing", http.StatusUnauthorized)
-// 		return
-// 	}
+	"github.com/google/uuid"
 
-// 	client := supabase.Client(supabaseUrl, supabaseKey, token)
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
-// 	query := r.URL.Query()
-// 	queryParams := make(map[string]string)
-// 	for key := range query {
-// 		// Modify the query parameter format to be compatible with Supabase
-// 		queryParams[key] = fmt.Sprintf("eq.%s", url.QueryEscape(query.Get(key)))
-// 	}
+var (
+	supabaseUrl = "https://your-project.supabase.co"
+	supabaseKey = "your-supabase-api-key"
+)
 
-// 	body, err := client.Get("Food", queryParams)
-// 	if err != nil {
-// 		http.Error(w, "Error fetching data from Supabase", http.StatusInternalServerError)
-// 		return
-// 	}
+type FoodCreate struct {
+	UserID     uuid.UUID `json:"user_id"`
+	Restaurant string    `json:"restaurant"`
+	Rating     int64     `json:"rating"`
+	FoodName   string    `json:"food_name"`
+	Opinion    string    `json:"opinion"`
+	Image      string    `json:"image"`
+}
 
-// 	w.Header().Set("Content-Type", "application/json")
-// 	_, err = w.Write(body)
-// 	if err != nil {
-// 		log.Printf("Error writing response: %v", err)
-// 	}
-// }
+type FoodUpdate struct {
+	Id         *int64    `json:"id"`
+	UserID     uuid.UUID `json:"user_id"`
+	Restaurant string    `json:"restaurant"`
+	Rating     int64     `json:"rating"`
+	FoodName   string    `json:"food_name"`
+	Opinion    string    `json:"opinion"`
+	Image      string    `json:"image"`
+}
 
-// func createFoodHandler(w http.ResponseWriter, r *http.Request) {
-// 	token := r.Header.Get("Authorization")
+func getFoodHandler(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("Authorization")
+	if token == "" {
+		http.Error(w, "Authorization token missing", http.StatusUnauthorized)
+		return
+	}
 
-// 	client := supabase.Client(supabaseBaseUrl, supabaseKey, token)
+	client := supabase.Client(supabaseUrl, supabaseKey, token)
 
-// 	var data map[string]interface{}
-// 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-// 		http.Error(w, "Invalid request body", http.StatusBadRequest)
-// 		return
-// 	}
+	query := r.URL.Query()
+	queryParams := make(map[string]string)
+	for key := range query {
+		// Modify the query parameter format to be compatible with Supabase
+		queryParams[key] = fmt.Sprintf("eq.%s", url.QueryEscape(query.Get(key)))
+	}
 
-// 	jsonData, err := json.Marshal(data)
-// 	if err != nil {
-// 		http.Error(w, "Failed to marshal JSON", http.StatusInternalServerError)
-// 		return
-// 	}
+	body, err := client.Get("Food", queryParams)
+	if err != nil {
+		http.Error(w, "Error fetching data from Supabase", http.StatusInternalServerError)
+		return
+	}
 
-// 	body, err := client.Post("food", jsonData)
-// 	if err != nil {
-// 		http.Error(w, fmt.Sprintf("Failed to create food data: %v", err), http.StatusInternalServerError)
-// 		return
-// 	}
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(body)
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
+}
 
-// 	w.Header().Set("Content-Type", "application/json")
-// 	_, err = w.Write(body)
-// 	if err != nil {
-// 		log.Printf("Error writing response: %v", err)
-// 	}
-// }
+func createFoodHandler(w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		http.Error(w, "Authorization token missing", http.StatusUnauthorized)
+		return
+	}
 
-// func updateFoodHandler(w http.ResponseWriter, r *http.Request) {
-// 	token := r.Header.Get("Authorization")
+	token := strings.TrimPrefix(authHeader, "Bearer ")
 
-// 	// Extract the item ID from the URL path
-// 	itemId := chi.URLParam(r, "itemId")
-// 	if itemId == "" {
-// 		http.Error(w, "Missing item ID", http.StatusBadRequest)
-// 		return
-// 	}
+	// ExtractUserId not included in example but this pull id from token
+	userID, err := utils.ExtractUserId(token)
+	if err != nil {
+		http.Error(w, "Invalid token", http.StatusUnauthorized)
+		return
+	}
 
-// 	client := supabase.Client(supabaseBaseUrl, supabaseKey, token)
+	client := supabase.Client(supabaseUrl, supabaseKey, authHeader)
 
-// 	var data map[string]interface{}
-// 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-// 		http.Error(w, "Invalid request body", http.StatusBadRequest)
-// 		return
-// 	}
+	var food FoodCreate
+	if err := json.NewDecoder(r.Body).Decode(&food); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
 
-// 	jsonData, err := json.Marshal(data)
-// 	if err != nil {
-// 		http.Error(w, "Failed to marshal JSON", http.StatusInternalServerError)
-// 		return
-// 	}
+	food.UserID = userID
 
-// 	body, err := client.Put(fmt.Sprintf("food?id=eq.%s", itemId), jsonData)
-// 	if err != nil {
-// 		http.Error(w, fmt.Sprintf("Failed to update food data: %v", err), http.StatusInternalServerError)
-// 		return
-// 	}
+	jsonData, err := json.Marshal(food)
+	if err != nil {
+		http.Error(w, "Failed to marshal JSON", http.StatusInternalServerError)
+		return
+	}
 
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.Write(body)
-// }
+	log.Printf("Request payload: %s", string(jsonData))
 
-// func deleteFoodHandler(w http.ResponseWriter, r *http.Request) {
-// 	token := r.Header.Get("Authorization")
+	body, err := client.Post("Food", jsonData)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to create food data: %v", err), http.StatusInternalServerError)
+		return
+	}
 
-// 	// Extract the item ID from the URL path
-// 	itemId := chi.URLParam(r, "itemId")
-// 	if itemId == "" {
-// 		http.Error(w, "Missing item ID", http.StatusBadRequest)
-// 		return
-// 	}
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(body)
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
+}
 
-// 	client := supabase.Client(supabaseBaseUrl, supabaseKey, token)
-// 	body, err := client.Delete(fmt.Sprintf("food?id=eq.%s", itemId))
-// 	if err != nil {
-// 		http.Error(w, fmt.Sprintf("Failed to delete food data: %v", err), http.StatusInternalServerError)
-// 		return
-// 	}
+// primary key need to be in request body for PUT
+// https://docs.postgrest.org/en/v12/references/api/tables_views.html#put
 
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.Write(body)
-// }
+func updateFoodHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		http.Error(w, "Authorization token missing", http.StatusUnauthorized)
+		return
+	}
+
+	token := strings.TrimPrefix(authHeader, "Bearer ")
+
+	// ExtractUserId not included in example but this pull id from token
+	userID, err := utils.ExtractUserId(token)
+	if err != nil {
+		http.Error(w, "Invalid token", http.StatusUnauthorized)
+		return
+	}
+
+	itemId := chi.URLParam(r, "itemId")
+	if itemId == "" {
+		http.Error(w, "Missing item ID", http.StatusBadRequest)
+		return
+	}
+
+	client := supabase.Client(supabaseUrl, supabaseKey, authHeader)
+
+	var food FoodUpdate
+	if err := json.NewDecoder(r.Body).Decode(&food); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	food.UserID = userID
+
+	// Ensure the primary key is included in the request body
+	foodId, err := strconv.ParseInt(itemId, 10, 64)
+	if err != nil {
+		http.Error(w, "Invalid item ID", http.StatusBadRequest)
+		return
+	}
+	food.Id = &foodId
+
+	jsonData, err := json.Marshal(food)
+	if err != nil {
+		http.Error(w, "Failed to marshal JSON", http.StatusInternalServerError)
+		return
+	}
+
+	url := fmt.Sprintf("Food?id=eq.%s", itemId)
+
+	body, err := client.Put(url, jsonData)
+	if err != nil {
+		log.Printf("Supabase PUT request error: %v", err)
+		http.Error(w, fmt.Sprintf("Failed to update food data: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(body)
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
+}
+
+func deleteFoodHandler(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("Authorization")
+
+	// Extract the item ID from the URL path
+	itemId := chi.URLParam(r, "itemId")
+	if itemId == "" {
+		http.Error(w, "Missing item ID", http.StatusBadRequest)
+		return
+	}
+
+	client := supabase.Client(supabaseUrl, supabaseKey, token)
+	body, err := client.Delete(fmt.Sprintf("food?id=eq.%s", itemId))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to delete food data: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(body)
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
+}
 
 func main() {
-	// r := chi.NewRouter()
-	// r.Use(middleware.Logger)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
-	// r.Route("/v1", func(r chi.Router) {
-	// 	r.Get("/food", getFoodHandler)
-	// 	r.Post("/food", createFoodHandler)
-	// 	r.Put("/food/{itemId}", updateFoodHandler)
-	// 	r.Delete("/food/{itemId}", deleteFoodHandler)
-	// })
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/food", getFoodHandler)
+		r.Post("/food", createFoodHandler)
+		r.Put("/food/{itemId}", updateFoodHandler)
+		r.Delete("/food/{itemId}", deleteFoodHandler)
+	})
 
-	// fmt.Println("Server is running on port 8080")
-	// http.ListenAndServe(":8080", r)
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", r)
 }
